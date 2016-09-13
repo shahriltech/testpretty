d8c5af27d4fe9c2efd217a3dc0ac41ca07e00954
shahril anuar <shahril.anuar@madlabs.com.my>
2016-09-13 23:05:02 +0800

    edit welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel 5.3
                 </div>
 
                 <div class="links">
